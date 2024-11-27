4ccc1cb0f2103102e1aa965e4f9415ee5e11b936
Evan McClure <evanmcclure@gmail.com>
2024-11-27 14:30:59 -0800

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::io::Error;
 
-use clap::{Args, Parser, Subcommand};
+use clap::{Parser, Subcommand};
 
 // Setup constants that control the neural network (NN)
 const DEFAULT_INPUT_NODES: u16 = 784;
@@ -14,6 +14,7 @@ const DEFAULT_MNIST_TEST_DATA_CSV_FILENAME: &str = "mnist_test.csv";
 const DEFAULT_MODEL_INPUT_FILENAME: &str = "model.bin";
 const DEFAULT_PNG_INPUT_FILENAME: &str = "image.png";
 
+// App commands
 #[derive(Debug, Parser)]
 #[clap(about, version)]
 struct App {
@@ -66,28 +67,96 @@ enum Commands {
     },
 }
 
-// Train the NN
-fn train(
+#[derive(Debug)]
+struct TrainConfig {
     input_nodes: u16,
     hidden_nodes: u16,
     output_nodes: u16,
     learning_rate: f32,
-    mnist_training_data_csv_filename: &str,
+    mnist_training_data_csv_filename: String,
     num_training_epochs: u16,
-    model_output_filename: &str,
-) -> Result<(), Error> {
+    model_output_filename: String,
+}
+
+impl TrainConfig {
+    pub fn new(
+        input_nodes: u16,
+        hidden_nodes: u16,
+        output_nodes: u16,
+        learning_rate: f32,
+        mnist_training_data_csv_filename: &str,
+        num_training_epochs: u16,
+        model_output_filename: &str,
+    ) -> Self {
+        TrainConfig {
+            input_nodes: input_nodes,
+            hidden_nodes: hidden_nodes,
+            output_nodes: output_nodes,
+            learning_rate: learning_rate,
+            mnist_training_data_csv_filename: String::from(mnist_training_data_csv_filename),
+            num_training_epochs: num_training_epochs,
+            model_output_filename: String::from(model_output_filename),
+        }
+    }
+}
+
+impl Default for TrainConfig {
+    fn default() -> Self {
+        Self {
+            input_nodes: DEFAULT_INPUT_NODES,
+            hidden_nodes: DEFAULT_HIDDEN_NODES,
+            output_nodes: DEFAULT_OUTPUT_NODES,
+            learning_rate: DEFAULT_LEARNING_RATE,
+            mnist_training_data_csv_filename: String::from(
+                DEFAULT_MNIST_TRAINING_DATA_CSV_FILENAME,
+            ),
+            num_training_epochs: DEFAULT_NUM_TRAINING_EPOCHS,
+            model_output_filename: String::from(DEFAULT_MODEL_OUTPUT_FILENAME),
+        }
+    }
+}
+
+// Train the NN
+fn train(config: TrainConfig) -> Result<(), Error> {
+    println!("Train config is {:#?}", config);
+
     // Create the NN
 
     // Load the training data CSV
 
     // Save the model
 
-    println!("train");
     Ok(())
 }
 
+#[derive(Debug)]
+struct TestConfig {
+    mnist_test_data_csv_filename: String,
+    model_input_filename: String,
+}
+
+impl TestConfig {
+    pub fn new(mnist_test_data_csv_filename: &str, model_input_filename: &str) -> Self {
+        TestConfig {
+            mnist_test_data_csv_filename: String::from(mnist_test_data_csv_filename),
+            model_input_filename: String::from(model_input_filename),
+        }
+    }
+}
+
+impl Default for TestConfig {
+    fn default() -> Self {
+        Self {
+            mnist_test_data_csv_filename: String::from(DEFAULT_MNIST_TEST_DATA_CSV_FILENAME),
+            model_input_filename: String::from(DEFAULT_MODEL_INPUT_FILENAME),
+        }
+    }
+}
+
 // Test the NN
-fn test(mnist_test_data_csv_filename: &str, model_input_filename: &str) -> Result<(), Error> {
+fn test(config: TestConfig) -> Result<(), Error> {
+    println!("Test config is {:#?}", config);
+
     // Load the model
 
     // Create the NN
@@ -96,26 +165,48 @@ fn test(mnist_test_data_csv_filename: &str, model_input_filename: &str) -> Resul
 
     // Calculate the performance score
 
-    println!("test");
     Ok(())
 }
 
+#[derive(Debug)]
+struct InferConfig {
+    model_input_filename: String,
+    png_input_filename: String,
+}
+
+impl InferConfig {
+    pub fn new(model_input_filename: &str, png_input_filename: &str) -> Self {
+        InferConfig {
+            model_input_filename: String::from(model_input_filename),
+            png_input_filename: String::from(png_input_filename),
+        }
+    }
+}
+
+impl Default for InferConfig {
+    fn default() -> Self {
+        Self {
+            model_input_filename: String::from(DEFAULT_MODEL_INPUT_FILENAME),
+            png_input_filename: String::from(DEFAULT_PNG_INPUT_FILENAME),
+        }
+    }
+}
+
 // Infer a new handwritten digit
-fn infer(model_input_filename: &str, png_input_filename: &str) -> Result<(), Error> {
+fn infer(config: InferConfig) -> Result<(), Error> {
+    println!("Inter config is {:#?}", config);
+
     // Load the model
 
     // Create the NN
 
     // Read an image and infer the digit
 
-    println!("infer");
     Ok(())
 }
 
 fn main() -> Result<(), Error> {
     let app = App::parse();
-
-    println!("app args are {:#?}", &app);
 
     match &app.command {
         Commands::Train {
@@ -126,22 +217,31 @@ fn main() -> Result<(), Error> {
             mnist_training_data_csv_filename,
             num_training_epochs,
             model_output_filename,
-        } => train(
-            *input_nodes,
-            *hidden_nodes,
-            *output_nodes,
-            *learning_rate,
-            mnist_training_data_csv_filename,
-            *num_training_epochs,
-            model_output_filename,
-        ),
+        } => {
+            let config = TrainConfig::new(
+                *input_nodes,
+                *hidden_nodes,
+                *output_nodes,
+                *learning_rate,
+                mnist_training_data_csv_filename,
+                *num_training_epochs,
+                model_output_filename,
+            );
+            train(config)
+        }
         Commands::Test {
             mnist_test_data_csv_filename,
             model_input_filename,
-        } => test(mnist_test_data_csv_filename, model_input_filename),
+        } => {
+            let config = TestConfig::new(mnist_test_data_csv_filename, model_input_filename);
+            test(config)
+        }
         Commands::Infer {
             model_input_filename,
             png_input_filename,
-        } => infer(model_input_filename, png_input_filename),
+        } => {
+            let config = InferConfig::new(model_input_filename, png_input_filename);
+            infer(config)
+        }
     }
 }
